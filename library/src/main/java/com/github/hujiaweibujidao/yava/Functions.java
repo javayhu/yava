7bd9b36b5ac939fceef90899aa2e2b6d85103ed4
潇涧 <hujiawei090807@gmail.com>
2016-05-31 22:03:37 +0800

    remove IFunction in Functions

--- library/src/main/java/com/github/hujiaweibujidao/yava/Functions.java
+++ library/src/main/java/com/github/hujiaweibujidao/yava/Functions.java
@@ -5,7 +5,6 @@ package com.github.hujiaweibujidao.yava;
  * 工具类，将自定义的函数快速封装成AbstractFunction
  */
 public class Functions {
-    private static IFunction function;
 
     public static AbstractFunction with(final IFunction function) {
         return new AbstractFunction() {
